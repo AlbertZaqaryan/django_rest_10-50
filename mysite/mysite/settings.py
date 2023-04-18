6035aa97e73aef4a3f2e4041cc1a496a43c41dc4
AlbertZaqaryan <itm.albert.zaqaryan@gmail.com>
2023-04-18 12:18:08 +0400

    MySQL connect

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'for_django_10-50',
+        'USER': 'root',
+        'PASSWORD': '1111',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"},
     }
 }
 
